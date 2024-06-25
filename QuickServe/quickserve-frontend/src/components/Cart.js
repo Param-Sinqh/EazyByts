9ca85b39fa89b9950d660d9f79e4c74521b6c9cf
Param <parmsinghc182@gmail.com>
2024-06-25 16:18:12 +0530

    styling cart page...

--- QuickServe/quickserve-frontend/src/components/Cart.js
+++ QuickServe/quickserve-frontend/src/components/Cart.js
@@ -1,31 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/Cart.css';
+import CustomDietLogo from './CustomDietLogo';
 
-const Cart = ({ cart, updateCart }) => {
+const Cart = ({ cart, updateCart, clearCart }) => {
     const handleQuantityChange = (item, quantity) => {
         updateCart(item, quantity);
     };
 
     return (
-        <div>
-            <h1>Cart</h1>
-            <div>
-                {cart.map((item, index) => (
-                    <div key={index}>
-                        <h2>{item.name}</h2>
-                        <p>{item.description}</p>
-                        <p>${item.price}</p>
-                        <input
-                            type="number"
-                            value={item.quantity}
-                            onChange={(e) => handleQuantityChange(item, e.target.value)}
-                            min="1"
-                        />
-                        <button onClick={() => handleQuantityChange(item, 0)}>Remove</button>
-                    </div>
-                ))}
+        <div className='cartPage'>
+            <div className='cart-container'>
+                <button className='clear-cart' onClick={clearCart}>Remove all</button>
+                <div className='cart-items'>
+                    {cart.map((item, index) => (
+                        <div key={index} className='cart-item'>
+                            <table>
+                                <tr>
+                                    <td>
+
+                                    </td>
+                                </tr>
+                            </table>
+                            <img src={item.image} alt={item.name} className='cart-item-image' />
+                            <div className='cart-item-details'>
+                                <div>
+                                    <div className='name-diet'>
+                                        <h2>{item.name}</h2>
+                                        <div className='diet-stamp'>
+                                            <div className='dietIcon'>
+                                                {item.veg ? <CustomDietLogo fill={"#12b800"} /> : <CustomDietLogo fill={"#b80000"} />}
+                                            </div>
+                                            {item.veg ? <p style={{ color: '#12b800' }}>Veg</p> : <p style={{ color: '#b80000' }}>Non Veg</p>}
+                                        </div>
+                                    </div>
+                                    <p>{item.description}</p>
+                                </div>
+                                <div className='cart-item-quantity'>
+                                    <button onClick={() => handleQuantityChange(item, item.quantity - 1)}>-</button>
+                                    <span>{item.quantity}</span>
+                                    <button onClick={() => handleQuantityChange(item, item.quantity + 1)}>+</button>
+                                </div>
+                                <p className='cart-item-price'>${item.price.toFixed(2)}</p>
+                                <div className='cart-item-actions'>
+                                    <button className='remove-item' onClick={() => handleQuantityChange(item, 0)}>Remove</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className='cart-summary'>
+                    <h3>Sub-Total</h3>
+                    <p>{cart.reduce((acc, item) => acc + item.quantity, 0)} items</p>
+                    <h3>${cart.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}</h3>
+                    <Link to="/address" className='checkout-button'>Checkout</Link>
+                </div>
             </div>
-            <Link to="/address">Proceed to Address</Link>
         </div>
     );
 };
--- QuickServe/quickserve-frontend/src/components/Navbar.js
+++ QuickServe/quickserve-frontend/src/components/Navbar.js
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import "./css/Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = ({ cart }) => {
     const [cartCount, setCartCount] = useState(0);
+    const location = useLocation();
 
     useEffect(() => {
         const count = cart.reduce((total, item) => total + item.quantity, 0);
         setCartCount(count);
     }, [cart]);
 
+    const getPageName = () => {
+        switch (location.pathname) {
+            case '/':
+                return 'QuickServe';
+            case '/cart':
+                return 'Cart';
+            default:
+                return 'QuickServe';
+        }
+    };
+
+    const handleClick = () => {
+        // Define your onClick functionality here
+        console.log("H2 clicked on home page");
+    };
+
     return (
         <nav>
-            <h2>QuickServe</h2>
+            <h2
+                onClick={location.pathname === '/' ? handleClick : undefined}
+                style={{ cursor: location.pathname === '/' ? 'pointer' : 'default' }}
+            >
+                {getPageName()}
+            </h2>
             <ul>
                 <li>
                     <Link to="/">
